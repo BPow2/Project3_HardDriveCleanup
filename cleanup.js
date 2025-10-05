2a2f362460897a83fe253a03ece67bbc0c5e5668
Bryan <b.pow2.bjp@gmail.com>
2025-10-04 18:10:21 -0600

    initial commit

---
+++ cleanup.js
@@ -0,0 +1,72 @@
+import fs from "node:fs";
+import path from "path";
+import {filesize} from "filesize";
+
+const startingFolder = "C:\\Users\\bpow2\\Projects\\Coding\\testDir"
+
+function fileStats(file) {
+    const size = getFileSize(file);
+    return filesize(size);
+}
+
+function getFileSize(file) {
+    return fs.statSync(file).size;
+}
+
+function readFile(fileName) {
+    try {
+        return fs.readFileSync(fileName, "utf8");
+    } catch (err) {
+        console.error("Error:", err);
+    }
+}
+
+function getDirTotal(dir) {
+    let dirTotalSize = 0;
+    const files = fs.readdirSync(dir);
+    files.forEach(file => {
+        dirTotalSize += getFileSize(file)
+    })
+    console.log(dirTotalSize);
+}
+
+function readDirRecursive(dir, parentDir = './') {
+    try {
+        const files = fs.readdirSync(dir);
+        files.forEach(file => {
+            const filePath = path.join(dir, file);
+            const stat = fs.statSync(filePath);
+            let currentDir = "./"
+            if (stat.isDirectory()) {
+                dirArr.push(
+                    {
+                        "path": filePath,
+                        "type": "directory" ,  
+                        "parent" : parentDir              
+                    }
+                )
+                readDirRecursive(filePath, filePath);
+            } else {
+                dirArr.push(
+                    {
+                        "name": file,
+                        "path": filePath,
+                        "type": "file",
+                        "size": fileStats(filePath),
+                        "parent": parentDir
+                    }
+                )   
+            }
+        });
+    } catch (err) {
+        console.error("Error reading directory:", dir, err);
+    }
+}
+
+if (process.argv.includes("-h") || process.argv.includes("--help")) {
+    console.log(readFile("./help.txt"));
+}
+
+let dirArr = []
+readDirRecursive(startingFolder);
+console.log(dirArr)
